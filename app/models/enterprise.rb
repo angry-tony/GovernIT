df0e869195c4b8ca7f6d447f767def416fc3efe5
Sebastián Lesmes Alvarado <slesmes1992@gmail.com>
2015-12-14 23:41:29 -0500

    - Delete user-enterprise relationship - Modify seeds file to provide a progress report of the process.

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -2,7 +2,6 @@ class Enterprise < ActiveRecord::Base
 	has_many :governance_decisions
     has_many :specific_risks, class_name: "Risk", inverse_of: :enterprise
 	has_many :governance_structures
-	has_many :users
 	has_one :configuracion, inverse_of: :enterprise, class_name: "Configuracion"
 
 	has_attached_file :logo, 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,11 @@
 
 # ========================================== DATABASE: DECISION MAPS ===========================
 
+puts 'Engine: Maps (1/2) - Loading seed data...'
+
 Mapas::Engine.load_seed
+
+puts 'Engine: Maps (1/2) - Seed data loaded: 100%'
 
 # ========================================== DATABASE: DECISION MAPS ===========================
 
@@ -13,6 +17,20 @@ Mapas::Engine.load_seed
 
 # ========================================== DATABASE: ASSESSMENT SCENARIOS ===========================
 
+puts 'Engine: Scenarios (2/2) - Loading seed data...'
+
 Escenarios::Engine.load_seed
 
+puts 'Engine: Scenarios (2/2) - Seed data loaded: 100%'
+
 # ========================================== DATABASE: ASSESSMENT SCENARIOS ===========================
+
+# Creates a dummy enterprise:
+
+dummyEnt = Enterprise.new
+dummyEnt.name = 'Test Enterprise'
+dummyEnt.description = 'Initial enterprise able to support all functionalities of the software.'
+dummyEnt.save
+
+
+puts '----- Seed data loaded successfully! -----'
